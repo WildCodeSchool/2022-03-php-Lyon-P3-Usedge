3f0c8ec00dfef6021cc6ac92b00453721f5ebda2
christopher cuzin <ed.ed1009@hotmail.fr>
2022-06-08 13:08:25 +0200

    factorization of js, add line for each research view, create partial file for scss variable, add link to requests templates view, change p tag by table tag

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-import './js/app.js'
+import './js/homePage.js'
 
 // start the Stimulus application
 import './bootstrap';
---
+++ assets/js/homePage.js
@@ -0,0 +1,70 @@
+const plans = document.getElementById('reasearch-plans');
+const requests = document.getElementById('reasearch-requests');
+const requestsList = document.getElementById('requests-list');
+const plansList = document.getElementById('plans-list');
+const sortByChoice = document.getElementsByClassName('sort-by-choice');
+const viewChoice = document.getElementsByClassName('view-choice');
+
+
+if (plans.checked == false && requests.checked == true || requests.checked == false && plans.checked == true) {        
+    requests.onchange = function() {
+        plans.checked = true;
+        if(requests.checked == true){
+            plansList.style.display = 'flex';
+            requestsList.style.display = 'flex';
+            requestsList.style.marginRight = '20px';
+            for (let i = 0; i < sortByChoice.length; i++) {
+                const element = sortByChoice[i];
+                element.style.display = 'none';
+            }
+            for (let i = 0; i < viewChoice.length; i++) {
+                const element = viewChoice[i];
+                element.style.display = 'none';
+            }
+        } else {
+            requestsList.style.display = 'none';
+            plansList.style.display = 'flex';
+            for (let i = 0; i < sortByChoice.length; i++) {
+                const element = sortByChoice[i];
+                element.style.display = 'block'; 
+            }
+            for (let i = 0; i < viewChoice.length; i++) {
+                const element = viewChoice[i];
+                element.style.display = 'flex';
+            }
+        
+        }
+    } 
+    plans.onchange = function() {
+        requests.checked = true;
+        if(plans.checked == true){
+            plansList.style.display = 'flex';
+            requestsList.style.display = 'flex';
+            requestsList.style.marginRight = '20px';
+            for (let i = 0; i < sortByChoice.length; i++) {
+                const element = sortByChoice[i];
+                element.style.display = 'none';
+            }
+            for (let i = 0; i < viewChoice.length; i++) {
+                const element = viewChoice[i];
+                element.style.display = 'none';
+            }
+        } else {
+            plansList.style.display = 'none';
+            requestsList.style.display = 'flex';
+            for (let i = 0; i < sortByChoice.length; i++) {
+                const element = sortByChoice[i];
+                element.style.display = 'block'; 
+            }
+            for (let i = 0; i < viewChoice.length; i++) {
+                const element = viewChoice[i];
+                element.style.display = 'flex';
+            }
+        }
+    }
+}
+ 
+
+    
+
+
