b0a4407ef766683fc73e153cfaa77abae57d6fbb
christopher cuzin <ed.ed1009@hotmail.fr>
2022-06-09 16:52:49 +0200

    modify html, css and js

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -1,74 +1,71 @@
-const plans = document.getElementById('reasearch-plans');
-const requests = document.getElementById('reasearch-requests');
+const researchPlans = document.getElementById('reasearch-plans');
+const researchRequests = document.getElementById('reasearch-requests');
 const requestsList = document.getElementById('requests-list');
-const plansList = document.getElementById('plans-list');
-const sortByChoice = document.getElementsByClassName('sort-by-choice');
-const viewChoice = document.getElementsByClassName('view-choice');
+const plansListNone = document.getElementById('plans-list-none');
+const PviewlansAssignedRequest = document.getElementById('view-plans-assigned-request');
+const viewPlansProject = document.getElementById('view-plans-project');
+const viewRequestCoachAssigned = document.getElementById('view-request-coach-assigned');
+const viewRequestsProject = document.getElementById('view-requests-project');
+const requestsProject = document.getElementById('requests-project');
+const requestsCoachAssigned = document.getElementById('requests-coach-assigned');
+const plansProject = document.getElementById('plans-project');
+const plansAssignedRequest = document.getElementById('plans-assigned-request');
 
-
-if (plans.checked == false && requests.checked == true || requests.checked == false && plans.checked == true) {        
-    requests.onchange = function() {
-        plans.checked = true;
-        if(requests.checked == true){
-            plansList.style.display = 'flex';
-            requestsList.style.display = 'flex';
-            requestsList.style.width = '49%';
-            plansList.style.width = '49%';
-            for (let i = 0; i < sortByChoice.length; i++) {
-                const element = sortByChoice[i];
-                element.style.display = 'none';
-            }
-            for (let i = 0; i < viewChoice.length; i++) {
-                const element = viewChoice[i];
-                element.style.display = 'none';
-            }
-        } else {
-            requestsList.style.display = 'none';
-            plansList.style.display = 'flex';
-            for (let i = 0; i < sortByChoice.length; i++) {
-                const element = sortByChoice[i];
-                element.style.display = 'block'; 
-            }
-            for (let i = 0; i < viewChoice.length; i++) {
-                const element = viewChoice[i];
-                element.style.display = 'flex';
-            }
-            requestsList.style.width = '100%';
-            plansList.style.width = '100%';
+  
+researchRequests.onchange = function() {
+    researchPlans.checked = true;
+    if(researchRequests.checked == true && researchPlans.checked == true){
+        plansListNone.className = 'share-plans-list';
+        requestsList.className = 'share-requests-list';
+        PviewlansAssignedRequest.className = 'view-details-none';
+        viewPlansProject.className = 'view-details-none' ;
+        viewRequestCoachAssigned.className = 'view-details-none';
+        viewRequestsProject.className = 'view-details-none';
+        requestsProject.className = 'sort-none';
+        requestsCoachAssigned.className = 'sort-none';
+        plansProject.className = 'sort-none';
+        plansAssignedRequest.className = 'sort-none';
         
-        }
-    } 
-    plans.onchange = function() {
-        requests.checked = true;
-        if(plans.checked == true){
-            plansList.style.display = 'flex';
-            requestsList.style.display = 'flex';
-            requestsList.style.width = '49%';
-            plansList.style.width = '49%';
-            for (let i = 0; i < sortByChoice.length; i++) {
-                const element = sortByChoice[i];
-                element.style.display = 'none';
-            }
-            for (let i = 0; i < viewChoice.length; i++) {
-                const element = viewChoice[i];
-                element.style.display = 'none';
-            }
-        } else {
-            plansList.style.display = 'none';
-            requestsList.style.display = 'flex';
-            for (let i = 0; i < sortByChoice.length; i++) {
-                const element = sortByChoice[i];
-                element.style.display = 'block'; 
-            }
-            for (let i = 0; i < viewChoice.length; i++) {
-                const element = viewChoice[i];
-                element.style.display = 'flex';
-            }
-            requestsList.style.width = '100%';
-            plansList.style.width = '100%';
-        }
+    } else {
+        requestsList.className = 'requests-list-none ';
+        plansListNone.className = 'plans-list';
+        PviewlansAssignedRequest.className = 'view-details';
+        viewPlansProject.className = 'view-details';
+        viewRequestCoachAssigned.className = 'view-details';
+        viewRequestsProject.className = 'view-details';
+        requestsProject.className = 'sort';
+        requestsCoachAssigned.className = 'sort';
+        plansProject.className = 'sort';
+        plansAssignedRequest.className = 'sort';
     }
+} 
+researchPlans.onchange = function() {
+    researchRequests.checked = true;
+    if(researchRequests.checked == true && researchPlans.checked == true){
+        plansListNone.className = 'share-plans-list';
+        requestsList.className = 'share-requests-list';
+        PviewlansAssignedRequest.className = 'view-details-none';
+        viewPlansProject.className = 'view-details-none' ;
+        viewRequestCoachAssigned.className = 'view-details-none';
+        viewRequestsProject.className = 'view-details-none ';
+        requestsProject.className = 'sort-none';
+        requestsCoachAssigned.className = 'sort-none';
+        plansProject.className = 'sort-none';
+        plansAssignedRequest.className = 'sort-none';
+    } else {
+        requestsList.className = 'requests-list';
+        plansListNone.className = 'plans-list-none';
+        PviewlansAssignedRequest.className = 'view-details';
+        viewPlansProject.className = 'view-details';
+        viewRequestCoachAssigned.className = 'view-details';
+        viewRequestsProject.className = 'view-details';
+        requestsProject.className = 'sort';
+        requestsCoachAssigned.className = 'sort';
+        plansProject.className = 'sort';
+        plansAssignedRequest.className = 'sort';
+    } 
 }
+
  
 
     
