e5a4229f9d73d18bfb2baab4184ac06ccf2fa85b
Cid-Jie <pcidjie@gmail.com>
2022-06-09 12:27:58 +0200

    remove css style in javascript and create a class for this

--- assets/js/component.js
+++ assets/js/component.js
@@ -2,26 +2,15 @@
 const button = document.getElementById('button_answer');
 button.addEventListener('click', function() {
     const divAnswer = document.getElementById('form_answer');
-    let newInput = document.createElement('input');
-    newInput.type = 'text';
-    newInput.placeholder = 'Answer';
-    newInput.className = 'form_answer';
-    newInput.name = 'form_answer';
-    newInput.style.width = '100%';
-    newInput.style.height = '42px';
-    newInput.style.border = '1px solid #ECEFFF';
-    newInput.style.borderRadius = '8px';
-    newInput.style.marginBottom = '20px';
-    newInput.style.display = 'flex';
-    newInput.style.flexDirection = 'column';
-    divAnswer.appendChild(newInput);
+    const inputAnswer = document.getElementById('input_answer');
+    const inputAnswerClone = inputAnswer.cloneNode(true);
+    inputAnswerClone.type = 'text';
+    divAnswer.appendChild(inputAnswerClone);    
 });
 
 /*Action to send all informations in the modal to the div Add Elements */
 const buttonActionComponent = document.getElementById('button_action_component');
-
-const buttonModal = document.getElementById('button_action_modal');
-    
+const buttonModal = document.getElementById('button_action_modal');   
 buttonActionComponent.addEventListener('click', function() {
 
     const modal = document.getElementById('modal');
@@ -32,9 +21,9 @@ buttonActionComponent.addEventListener('click', function() {
     displayFormBuilder.innerHTML = displayFormBuilder.innerHTML + "<input type='hidden' name='input_question' value=" + inputQuestion + ">" + inputQuestion + "</br>";
     document.getElementById('input_question').value = ''; 
 
-    for (let i = 0; i < formAnswers.length; i++) {
+    for (let i = 1; i < formAnswers.length; i++) {
         const formAnswer = formAnswers[i].value;
-        displayFormBuilder.innerHTML = displayFormBuilder.innerHTML + "<input type='checkbox' class='checkboxAnswer' name='form_answer' value=" + formAnswer + [i] + ">" + formAnswer;
+        displayFormBuilder.innerHTML = displayFormBuilder.innerHTML + "<input type='checkbox' class='checkboxAnswer' name='form_answer' value=" + formAnswer + ">" + formAnswer;
         document.getElementById('form_answer').value = '';
     }
     modal.style.visibility = 'hidden';
