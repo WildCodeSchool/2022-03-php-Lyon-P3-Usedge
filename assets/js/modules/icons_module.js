f19882c41d680b2ed7e7eefb204287058cb1265b
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-06-09 15:25:08 +0200

    Changing path in icon modules js

--- assets/js/modules/icons_module.js
+++ assets/js/modules/icons_module.js
@@ -1,9 +1,9 @@
-import firstIcon from '../images/icons/template_icon_0.png';
-import secondIcon from '../images/icons/template_icon_1.png';
-import thirdIcon from '../images/icons/template_icon_2.png';
-import fourthIcon from '../images/icons/template_icon_3.png';
-import fifthIcon from '../images/icons/template_icon_4.png';
-import sixthIcon from '../images/icons/template_icon_5.png';
+import firstIcon from '../../images/icons/template_icon_0.png';
+import secondIcon from '../../images/icons/template_icon_1.png';
+import thirdIcon from '../../images/icons/template_icon_2.png';
+import fourthIcon from '../../images/icons/template_icon_3.png';
+import fifthIcon from '../../images/icons/template_icon_4.png';
+import sixthIcon from '../../images/icons/template_icon_5.png';
 
 // Array of imported icons from build.
 export let iconList = [
