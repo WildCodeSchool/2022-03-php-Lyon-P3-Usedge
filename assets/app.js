ca54608afe945c20e82e5fdb1b83009ac32b44de
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-06-09 14:46:31 +0200

    Creation of the js scripts for the new research template details modal. Creation of a js module folder to group icons imports.

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,9 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-import './js/add_research_template.js'
+import './js/modules/icons_module.js';
+import './js/new_research_template_details_modal.js';
+import './js/add_research_template.js';
 
 // start the Stimulus application
 import './bootstrap';
