14009a3873413ae298efb338943c5c4ec2bb9e9c
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-06-09 15:50:14 +0200

    merge dev + new migration

--- assets/app.js
+++ assets/app.js
@@ -7,13 +7,15 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import './js/add_research_template.js';
+import './js/component.js';
 import './js/modules/icons_module.js';
 import './js/new_research_template_details_modal.js';
-import './js/add_research_template.js';
+
 
 // start the Stimulus application
 import './bootstrap';
 
 require('bootstrap');
 
-import './js/component.js';
+
