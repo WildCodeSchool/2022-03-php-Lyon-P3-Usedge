9f65c1bc51c4221fae07bc6a4b71a8068b471144
MrGreen0000 <naji.mouflih@gmail.com>
2022-06-09 10:54:45 +0200

    I has creat a file of my css

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import './styles/newTemp.scss';
 require('bootstrap');
 
 // start the Stimulus application
