fdeec1cb4d47fa6c17aed24a8132c5fb52a6b99a
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-06-12 20:54:27 +0200

    Integration of the scale evaluation modal and the scale evaluation component on the templates creation page. Multiple components can be created. All components have uniques id's. A draggable attributes is present for next US.

--- assets/js/modals/evaluation_scale.js
+++ assets/js/modals/evaluation_scale.js
@@ -1,11 +1,45 @@
+
 if (document.getElementById('evaluation-scale-button')) {
     
     const evaluationScaleButton = document.getElementById('evaluation-scale-button')
     const evaluationScaleModal = document.getElementById('evaluation-scale-creation-modal');
+    const evaluationScaleCreateButton = document.getElementById('evaluation-scale-modal-button');
+    const formBuilder = document.getElementById('display_form_builder');
+    let idComponent = 0;
 
     //function used to open evaluation scale creation modal
     evaluationScaleButton.addEventListener('click', () => {
         evaluationScaleModal.classList.add('evaluation-scale-creation-modal-display');
-    })
+    });
 
+    //function used to create multiples template componants with different id's
+    evaluationScaleCreateButton.addEventListener('click', () => {
+        const evaluationScaleModalQuestion = document.getElementById('evaluation-scale-question-input').value;
+        const evaluationScaleModalLowLabel = document.getElementById('evaluation-scale-low-level-input').value;
+        const evaluationScaleModalHighLabel = document.getElementById('evaluation-scale-high-level-input').value;
+        const evaluationScaleModalMandatory = document.getElementById('evaluation-scale-checkbox-mandatory').checked;    
+        let evaluationScaleComponent = document.getElementById('evaluation-scale-template-creation');
+        let cloneComponent = evaluationScaleComponent.cloneNode(true);
+        const idElements = [...cloneComponent.querySelectorAll('[id]')];
+        if (cloneComponent.matches('[id]')) {
+            idElements.push(cloneComponent);
+        }
+        idElements.forEach((e) => {
+            e.id += '-' + idComponent;
+        })
+        cloneComponent.querySelector('.evaluation-scale-template-question').innerHTML = `${evaluationScaleModalQuestion}`;
+        cloneComponent.querySelector('.evaluation-scale-template-low-label').innerHTML = `${evaluationScaleModalLowLabel}`;
+        cloneComponent.querySelector('.evaluation-scale-template-high-label').innerHTML = `${evaluationScaleModalHighLabel}`;
+        if (evaluationScaleModalMandatory === true) {
+            cloneComponent.querySelector('.evaluation-scale-template-question').innerHTML = `${evaluationScaleModalQuestion}<span class="evaluation-scale-red-star" id="evaluation-scale-red-star">*</span>`;
+        }
+        cloneComponent.classList.add('evaluation-scale-template-display');
+        formBuilder.appendChild(cloneComponent);
+        evaluationScaleModal.classList.remove('evaluation-scale-creation-modal-display');
+        document.getElementById('evaluation-scale-question-input').value = "";
+        document.getElementById('evaluation-scale-low-level-input').value = "";
+        document.getElementById('evaluation-scale-high-level-input').value = "";
+        document.getElementById('evaluation-scale-checkbox-mandatory').checked = false;
+        idComponent++;
+    })
 }
