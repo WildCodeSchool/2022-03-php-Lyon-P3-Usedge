e768888011c2a8b0b7b259ac8a93396f4dd05e0d
Cid-Jie <pcidjie@gmail.com>
2022-06-15 16:23:38 +0200

    add display to see the title in the form builder

--- src/Controller/ResearchTemplateController.php
+++ src/Controller/ResearchTemplateController.php
@@ -9,6 +9,7 @@ use App\Form\ResearchTemplateType;
 use App\Form\SectionType;
 use App\Repository\ResearchTemplateRepository;
 use App\Repository\SectionRepository;
+use App\Repository\TemplateComponentRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,7 +43,8 @@ class ResearchTemplateController extends AbstractController
         Request $request,
         SectionRepository $sectionRepository,
         ResearchTemplate $researchTemplate,
-        ManagerRegistry $doctrine
+        ManagerRegistry $doctrine,
+        TemplateComponentRepository $tempCompRepository,
     ): Response {
         $section = new Section();
         $templateComponent = new TemplateComponent();
@@ -54,20 +56,23 @@ class ResearchTemplateController extends AbstractController
             $id = $researchTemplate->getId();
             $templateComponent->setResearchTemplate($researchTemplate);
             $templateComponent->setComponent($section);
-            $templateComponent->setNumberOrder(1);
+            $orderNumber = 1;
+            $templateComponent->setNumberOrder(++$orderNumber);
 
             $entityManager->persist($templateComponent);
-            $entityManager->persist($section);
             $sectionRepository->add($section, true);
             $entityManager->flush();
 
             return $this->redirectToRoute('research_template_add', ['id' => $id], Response::HTTP_SEE_OTHER);
         }
 
+        $templateComponents = $tempCompRepository->findAll();
+
         return $this->renderForm('research_template/add.html.twig', [
             'form' => $form,
             'section' => $section,
-            'researchTemplate' => $researchTemplate
+            'researchTemplate' => $researchTemplate,
+            'templateComponents' => $templateComponents,
         ]);
     }
 }
