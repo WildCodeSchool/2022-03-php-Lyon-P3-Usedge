de5a432d3c5d9a999c4e106fc00503ecd66fc5f5
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-06-15 20:29:22 +0200

    Deleting useless js code for the evaluation scale component. Min fix of the evaluation scale template creation css

--- assets/js/modals/evaluation_scale.js
+++ assets/js/modals/evaluation_scale.js
@@ -11,35 +11,4 @@ if (document.getElementById('evaluation-scale-button')) {
     evaluationScaleButton.addEventListener('click', () => {
         evaluationScaleModal.classList.add('evaluation-scale-creation-modal-display');
     });
-    /*
-    //function used to create multiples template componants with different id's
-    evaluationScaleCreateButton.addEventListener('click', () => {
-        const evaluationScaleModalQuestion = document.getElementById('evaluation-scale-question-input').value;
-        const evaluationScaleModalLowLabel = document.getElementById('evaluation-scale-low-level-input').value;
-        const evaluationScaleModalHighLabel = document.getElementById('evaluation-scale-high-level-input').value;
-        const evaluationScaleModalMandatory = document.getElementById('evaluation-scale-checkbox-mandatory').checked;    
-        const evaluationScaleComponent = document.getElementById('evaluation-scale-template-creation');
-        const cloneComponent = evaluationScaleComponent.cloneNode(true);
-        const idElements = [...cloneComponent.querySelectorAll('[id]')];
-        if (cloneComponent.matches('[id]')) {
-            idElements.push(cloneComponent);
-        }
-        idElements.forEach((e) => {
-            e.id += '-' + idComponent;
-        })
-        cloneComponent.querySelector('.evaluation-scale-template-question').innerHTML = `${evaluationScaleModalQuestion}`;
-        cloneComponent.querySelector('.evaluation-scale-template-low-label').innerHTML = `${evaluationScaleModalLowLabel}`;
-        cloneComponent.querySelector('.evaluation-scale-template-high-label').innerHTML = `${evaluationScaleModalHighLabel}`;
-        if (evaluationScaleModalMandatory === true) {
-            cloneComponent.querySelector('.evaluation-scale-template-question').innerHTML = `${evaluationScaleModalQuestion}<span class="evaluation-scale-red-star" id="evaluation-scale-red-star">*</span>`;
-        }
-        cloneComponent.classList.add('evaluation-scale-template-display');
-        formBuilder.appendChild(cloneComponent);
-        evaluationScaleModal.classList.remove('evaluation-scale-creation-modal-display');
-        document.getElementById('evaluation-scale-question-input').value = "";
-        document.getElementById('evaluation-scale-low-level-input').value = "";
-        document.getElementById('evaluation-scale-high-level-input').value = "";
-        document.getElementById('evaluation-scale-checkbox-mandatory').checked = false;
-        idComponent++;
-    })*/
 }
