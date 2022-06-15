2e3c440c6e93d5b189d7b3da04144a4386b513d5
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-06-15 19:25:39 +0200

    Integration in the twig page of research template creation, of the new components. Disappearence of the empty message when a first component is displayed.

--- src/Controller/ResearchTemplateController.php
+++ src/Controller/ResearchTemplateController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\ResearchTemplate;
 use App\Form\ResearchTemplateType;
 use App\Repository\ResearchTemplateRepository;
+use App\Repository\TemplateComponentRepository;
 use App\Services\ComponentFixtures;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -36,7 +37,8 @@ class ResearchTemplateController extends AbstractController
     public function add(
         Request $request,
         ResearchTemplate $researchTemplate,
-        ComponentFixtures $componentFixtures
+        ComponentFixtures $componentFixtures,
+        TemplateComponentRepository $tempCompRepository,
     ): Response {
 
         $componantName = $request->request->get('name');
@@ -47,16 +49,19 @@ class ResearchTemplateController extends AbstractController
                     $dataComponant = array_map('trim', $request->request->all());
                     $componentFixtures->loadEvaluationScale($dataComponant, $researchTemplate);
                     break;
-                default:
-                    echo 'error'; // CHANGE THIS
             }
+            $id = $researchTemplate->getId();
+
+            return $this->redirectToRoute('research_template_add', ['id' => $id], Response::HTTP_SEE_OTHER);
         }
 
         $validationErrors = $componentFixtures->getCheckErrors();
+        $templateComponents = $tempCompRepository->findBy(['researchTemplate' => $researchTemplate->getId()]);
 
         return $this->render('research_template/add.html.twig', [
             'researchTemplate' => $researchTemplate,
             'errors' => $validationErrors,
+            'templateComponents' => $templateComponents,
         ]);
     }
 }
