a48304b0ddaced6d0d18cfcc7829d8f801644814
Cid-Jie <pcidjie@gmail.com>
2022-06-19 17:38:37 +0200

    USRC-14 Add modal in twig

--- src/Entity/ExternalLink.php
+++ src/Entity/ExternalLink.php
@@ -4,24 +4,8 @@ namespace App\Entity;
 
 use App\Repository\ExternalLinkRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ExternalLinkRepository::class)]
 class ExternalLink extends Component
 {
-    #[ORM\Column(type: 'text')]
-    #[Assert\NotBlank(message: 'This field is mandatory.')]
-    private string $link;
-
-    public function getLink(): ?string
-    {
-        return $this->link;
-    }
-
-    public function setLink(string $link): self
-    {
-        $this->link = $link;
-
-        return $this;
-    }
 }
