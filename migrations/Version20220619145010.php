4b4aee8a5a13611ad2eee3266c9637cc63b9c8c8
Cid-Jie <pcidjie@gmail.com>
2022-06-19 16:57:59 +0200

    USRC-14 add entity external link

--- migrations/Version20220619145010.php
+++ migrations/Version20220619145010.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220616120313 extends AbstractMigration
+final class Version20220619145010 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220616120313 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE component DROP is_multiple');
+        $this->addSql('ALTER TABLE component ADD link LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE component ADD is_multiple TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE component DROP link');
     }
 }
