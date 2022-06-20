9cad7e0c9cf814c432ad18ae8537efb80fe9d578
christopher cuzin <ed.ed1009@hotmail.fr>
2022-06-20 11:34:16 +0200

    delete dead code

--- assets/js/modals/date-picker-modal.js
+++ assets/js/modals/date-picker-modal.js
@@ -9,14 +9,12 @@ if (document.getElementById('add-date-picker-button')) {
 
     // Function used to open the modal
     addDatePickerModalOpenButton.addEventListener('click', () => {
-        addDatePickerModal.classList.add('add-date-picker-modal-display');
         FullScreenDatePickerModalClose.classList.add('full-screen-date-picker-modal-display');
         body.classList.add('hide-body-overflow-date-picker');
         new draggable(addDatePickerModal, draggableOptions);
         // Function used to close the modal when click outside the modal
         window.onclick = function(event) {
             if (event.target == FullScreenDatePickerModalClose) {
-                addDatePickerModal.classList.remove('add-date-picker-modal-display'); 
                 FullScreenDatePickerModalClose.classList.remove('full-screen-date-picker-modal-display');
                 body.classList.remove('hide-body-overflow-date-picker');
             }
