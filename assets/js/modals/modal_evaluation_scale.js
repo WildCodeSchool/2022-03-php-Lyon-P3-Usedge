97e5cc0ab66d33bed19c16d0bd839e4b6b0b9d30
Cid-Jie <pcidjie@gmail.com>
2022-06-21 21:24:30 +0200

    Add css in the modal section

--- assets/js/modals/modal_evaluation_scale.js
+++ assets/js/modals/modal_evaluation_scale.js
@@ -4,11 +4,15 @@ if (document.getElementById('evaluation-scale-button')) {
     const evaluationScaleButton = document.getElementById('evaluation-scale-button')
     const evaluationScaleModal = document.getElementById('evaluation-scale-creation-modal');
     const evaluationScaleCreateButton = document.getElementById('evaluation-scale-modal-button');
-    const formBuilder = document.getElementById('display_form_builder');
-    let idComponent = 0;
+    //const formBuilder = document.getElementById('display_form_builder');
+    const evaluationScaleDraggableHandle = document.getElementById('evaluation-scale-draggable-handle');
+    const body = document.getElementById('body');
+    const draggable = require('draggable');
+    const draggableOptions = {handle: evaluationScaleDraggableHandle}
 
     //function used to open evaluation scale creation modal
     evaluationScaleButton.addEventListener('click', () => {
         evaluationScaleModal.classList.add('evaluation-scale-creation-modal-display');
+        new draggable(evaluationScaleModal, draggableOptions);
     });
 }
