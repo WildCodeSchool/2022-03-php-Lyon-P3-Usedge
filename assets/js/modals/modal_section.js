d443588b1d16d11412a70554d7bf0edb2788de53
Cid-Jie <pcidjie@gmail.com>
2022-06-21 11:56:03 +0200

    add draggable in section modal

--- assets/js/modals/modal_section.js
+++ assets/js/modals/modal_section.js
@@ -1,8 +1,12 @@
 /*Action to call all classes to active the section modal*/
 const modalSectionContainer = document.querySelector('.modal-section-container');
 const modalSectionTriggers = document.querySelectorAll('.modal-section-trigger');
+const sectionDraggableHandle = document.getElementById('section-draggable-handle');
+const draggable = require('draggable');
+const draggableOptions = {handle:sectionDraggableHandle}
 
 modalSectionTriggers.forEach(trigger => trigger.addEventListener('click', toggleSectionModal))
 function toggleSectionModal() {
     modalSectionContainer.classList.toggle('active');
+    new draggable(modalSectionContainer, draggableOptions);
 }
