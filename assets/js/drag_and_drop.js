327d845bb08b1e3e31d6f8dcb05dba990d934e88
christopher cuzin <ed.ed1009@hotmail.fr>
2022-06-21 18:14:20 +0200

    USRC-15 add draggable on separator

--- assets/js/drag_and_drop.js
+++ assets/js/drag_and_drop.js
@@ -3,10 +3,9 @@ import Sortable from 'sortablejs';
 if (document.getElementById('display_form_builder')) {
 
     const displayFormBuilder = document.getElementById('display_form_builder');
-    const handleTemplateComponentDraggable = document.getElementById('handle-template-component-draggable');
 
     Sortable.create(displayFormBuilder, {
-        handle: handleTemplateComponentDraggable,
+        handle: '.handle-template-component-draggable'
     });
     
 }
