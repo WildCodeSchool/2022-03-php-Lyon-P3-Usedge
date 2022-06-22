e230d9dc771ec82f96ad51774f31a97bed220a86
Cid-Jie <pcidjie@gmail.com>
2022-06-22 11:57:17 +0200

    Add javascript in modal evaluation scale

--- assets/js/modals/modal_evaluation_scale.js
+++ assets/js/modals/modal_evaluation_scale.js
@@ -1,10 +1,16 @@
-
+/*Action to call all classes to active the section modal*/
 if (document.getElementById('evaluation-scale-button')) {
-    
     const evaluationScaleButton = document.getElementById('evaluation-scale-button')
+    const FullScreenEvaluationScaleModalClose = document.getElementById('full-screen-evaluation-scale-modal-close');
+    const addEvaluationScaleModal = document.getElementById('add-evaluation-scale-modal');
+    const evaluationScaleModalCloseButton = document.getElementById('evaluation-scale-modal-close');
+    const inputAnswers = document.getElementsByTagName('input');
+    const inputMandatoryEvaluationScale = document.getElementById('evaluation-scale-checkbox-mandatory');
+
     const evaluationScaleModal = document.getElementById('evaluation-scale-creation-modal');
-    const evaluationScaleCreateButton = document.getElementById('evaluation-scale-modal-button');
-    //const formBuilder = document.getElementById('display_form_builder');
+    //const evaluationScaleModalContent = document.getElementById('evaluation-scale-creation-content');
+    //const evaluationScaleCreateButton = document.getElementById('evaluation-scale-modal-button');
+    //const evaluationScaleModalClose= document.getElementById('evaluation-scale-modal-close');
     const evaluationScaleDraggableHandle = document.getElementById('evaluation-scale-draggable-handle');
     const body = document.getElementById('body');
     const draggable = require('draggable');
@@ -12,7 +18,25 @@ if (document.getElementById('evaluation-scale-button')) {
 
     //function used to open evaluation scale creation modal
     evaluationScaleButton.addEventListener('click', () => {
-        evaluationScaleModal.classList.add('evaluation-scale-creation-modal-display');
-        new draggable(evaluationScaleModal, draggableOptions);
+        FullScreenEvaluationScaleModalClose.classList.add('full-screen-evaluation-scale-modal-display');
+        body.classList.add('hide-body-overflow-evaluation-scale');
+        new draggable(addEvaluationScaleModal, draggableOptions);
+        // Function used to close the modal when click outside of the modal
+        window.onclick = function(event) {
+            if (event.target == FullScreenEvaluationScaleModalClose) {
+                FullScreenEvaluationScaleModalClose.classList.remove('full-screen-evaluation-scale-modal-display');
+                body.classList.remove('hide-body-overflow-evaluation-scale');
+            }
+        };
     });
+
+    // Function used to close the modal when click on close button and initialise values
+    evaluationScaleModalCloseButton.addEventListener('click', () => {
+        for (const inputAnswer of inputAnswers) {
+            inputAnswer.value = '';
+        }
+        FullScreenEvaluationScaleModalClose.classList.remove('full-screen-evaluation-scale-modal-display');
+        body.classList.remove('hide-body-overflow-evaluation-scale');
+        inputMandatoryEvaluationScale.checked = false;
+    });  
 }
