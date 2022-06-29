e7c435bfd14108fa118015ae02e135042b84d565
christopher cuzin <ed.ed1009@hotmail.fr>
2022-06-29 08:25:08 +0200

    add new fields in ResearchRequest entity

--- src/Entity/ResearchRequest.php
+++ src/Entity/ResearchRequest.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ResearchRequestRepository;
+use DateTime;
+use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -20,6 +22,12 @@ class ResearchRequest
 
     #[ORM\OneToMany(mappedBy: 'researchRequest', targetEntity: AnswerRequest::class)]
     private Collection $answerRequests;
+
+    #[ORM\Column(type: 'datetime')]
+    private DateTimeInterface $creationDate;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $status;
 
     public function __construct()
     {
@@ -69,6 +77,30 @@ class ResearchRequest
                 $answerRequest->setResearchRequest(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
 
         return $this;
     }
