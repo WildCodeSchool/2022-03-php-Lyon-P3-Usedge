fc4d9f63e16a501501fef814effbd656c9f61883
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-07-02 15:45:59 +0200

    In ResearchRequestController : Retrieve one template ordered by numberOrder

--- src/Controller/ResearchRequestController.php
+++ src/Controller/ResearchRequestController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\ResearchTemplate;
+use App\Repository\TemplateComponentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ResearchRequestController extends AbstractController
 {
     #[Route('/research-request/add', name: 'research_request_add')]
-    public function index(): Response
+    public function index(TemplateComponentRepository $tempCompRepository): Response
     {
+        //$researchRequest = $tempCompRepository->findBy(['researchTemplate' => '28'], ['numberOrder' => 'ASC']);
+
         return $this->render('research_request/add.html.twig', [
             'controller_name' => 'ResearchRequestController',
         ]);
