a43c18e58048d69fe4ead7b654ab272f6518638c
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-07-03 15:18:55 +0200

    Add Open Question component to research request form

--- assets/js/add_research_request.js
+++ assets/js/add_research_request.js
@@ -1,6 +1,22 @@
 if (document.getElementById('add-research-request-header')) {
 
     const bodyWithSideBar = document.querySelector('body');
+
     bodyWithSideBar.classList.remove('body');
     bodyWithSideBar.classList.add('body-without-sidebar');
+
+    if (document.getElementById('research-open-question-text-area')) {
+        const textareas = document.querySelectorAll('.research-open-question-text-area');
+        
+        textareas.forEach(textarea => {
+            textarea.addEventListener('keydown', autosize);
+             
+            function autosize(){
+                var el = this;
+                setTimeout(function(){
+                    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+                },0);
+            }
+        });
+    }
 }
