145044bca1b76f81955ae63733021286ba19ef5c
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-07-04 14:13:04 +0200

    add modal hidden when clic out the modal

--- assets/js/modals/modal_action_menu_research_template.js
+++ assets/js/modals/modal_action_menu_research_template.js
@@ -9,12 +9,22 @@ if (document.getElementById('research-template-list-card')) {
 
         const buttonListCardOptions = document.getElementById('research-template-list-card-options' + i);
         const actionsMenuContainer = document.getElementById('actions-menu-container' + i);
+        const fullScreenActionMenuContainerModalClose = document.getElementById('full-screen-action-menu-container-modal-close' + i);
 
         // Open the modal actionsMenuContainer when click on buttonListCardOptions
         buttonListCardOptions.addEventListener('click', function () {
             actionsMenuContainer.classList.add('actions-menu-container-flex');
-        });
+            fullScreenActionMenuContainerModalClose.classList.add('full-screen-action-menu-container-modal-close-display');
 
+            // Function used to close the modal when click outside the modal
+
+            window.onclick = function (e) {
+                if (e.target == fullScreenActionMenuContainerModalClose) {
+                    actionsMenuContainer.classList.remove('actions-menu-container-flex');
+                    fullScreenActionMenuContainerModalClose.classList.remove('full-screen-action-menu-container-modal-close-display');
+                }
+            }
+        });
         i++;
     }
 }
