14140eb33555abc9c2789322345025ebd3b6c591
Anthony Gouton <anthony.gouton.wcs@gmail.com>
2022-07-04 16:37:32 +0200

    delete modal selector in app.js

--- assets/app.js
+++ assets/app.js
@@ -22,7 +22,6 @@ import './js/modals/modal_single_choice.js';
 import './js/modals/modal_evaluation_scale.js';
 import './js/modals/modal_section.js';
 import './js/modals/modal_date_picker.js';
-import './js/modals/modal_selector';
 import './js/modals/modal_action_menu_research_template';
 
 // Bootstrap import
