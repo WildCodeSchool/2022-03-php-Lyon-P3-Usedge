8c5db7523c26904d5af869c39f0dae734a0fc338
christopher cuzin <ed.ed1009@hotmail.fr>
2022-07-07 08:35:08 +0200

    USRC-35 get reference typing

--- src/DataFixtures/AnswerRequestFixtures.php
+++ src/DataFixtures/AnswerRequestFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\AnswerRequest;
+use App\Entity\ResearchRequest;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -10,8 +11,11 @@ use Faker\Factory;
 
 class AnswerRequestFixtures extends Fixture implements DependentFixtureInterface
 {
+    private ResearchRequest $researchRequest;
+
     public function load(ObjectManager $manager): void
     {
+
         $faker = Factory::create();
         $names = [
             'section',
@@ -26,10 +30,11 @@ class AnswerRequestFixtures extends Fixture implements DependentFixtureInterface
         for ($r = 1; $r <  46; $r++) {
             for ($a = 0; $a < rand(1, 3); $a++) {
                 foreach ($names as $name) {
+                    $this->researchRequest = $this->getReference('researchRequest_' . $r);
                     if ($name === 'multiple-choice') {
                         for ($m = 0; $m < rand(2, 4); $m++) {
                             $answerRequest = new AnswerRequest();
-                            $answerRequest->setResearchRequest($this->getReference('researchRequest_' . $r));
+                            $answerRequest->setResearchRequest($this->researchRequest);
                             $answerRequest->setName($name);
                             $answerRequest->setAnswer($faker->sentence(1));
                             $answerRequest->setQuestion($faker->sentence(1));
@@ -37,7 +42,7 @@ class AnswerRequestFixtures extends Fixture implements DependentFixtureInterface
                         }
                     } else {
                         $answerRequest = new AnswerRequest();
-                        $answerRequest->setResearchRequest($this->getReference('researchRequest_' . $r));
+                        $answerRequest->setResearchRequest($this->researchRequest);
                         $answerRequest->setName($name);
                         $answerRequest->setAnswer($faker->sentence(1));
                         $answerRequest->setQuestion($faker->sentence(1));
--- src/DataFixtures/ResearchRequestFixtures.php
+++ src/DataFixtures/ResearchRequestFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\ResearchRequest;
+use App\Entity\ResearchTemplate;
 use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -11,6 +12,8 @@ use Faker\Factory;
 
 class ResearchRequestFixtures extends Fixture implements DependentFixtureInterface
 {
+    private ResearchTemplate $researchTemplate;
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
@@ -19,9 +22,10 @@ class ResearchRequestFixtures extends Fixture implements DependentFixtureInterfa
         $requestNumber = 1;
         for ($t = 1; $t <  16; $t++) {
             for ($r = 0; $r <  3; $r++) {
+                $this->researchTemplate = $this->getReference('researchTemplate_' . $t);
                 $date = new DateTime();
                 $researchRequest = new ResearchRequest();
-                $researchRequest->setResearchTemplate($this->getReference('researchTemplate_' . $t));
+                $researchRequest->setResearchTemplate($this->researchTemplate);
                 $researchRequest->setCreationDate($date);
                 $researchRequest->setStatus($status[array_rand($status)]);
                 $researchRequest->setProject('Webb app');
