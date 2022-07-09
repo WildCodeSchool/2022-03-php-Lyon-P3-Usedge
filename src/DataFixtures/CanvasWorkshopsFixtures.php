8d80c24d7bdded2206f8645575e3f99080bed84d
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-07-09 07:19:49 +0200

    Creation of the workshop constant with all workshops and canvas available for usedge

--- src/DataFixtures/CanvasWorkshopsFixtures.php
+++ src/DataFixtures/CanvasWorkshopsFixtures.php
@@ -7,8 +7,159 @@ use Doctrine\Persistence\ObjectManager;
 
 class CanvasWorkshopsFixtures extends Fixture
 {
+    private const WORKSHOPS = [
+        [
+            'name' => 'Preference testing',
+            'description' => 'Put a new interface in competition
+                                with a control interface and evaluate the
+                                performance of one against the other.',
+            'picture' => 'build/images/logo_workshop/preference-testing.png',
+            'type' => 'workshop',
+        ],
+        [
+            'name' => 'Value proposition',
+            'description' => 'Put a new interface in competition
+                                with a control interface and evaluate the
+                                performance of one against the other.',
+            'picture' => 'build/images/logo_workshop/preference-testing.png',
+            'type' => 'canva',
+        ],
+        [
+            'name' => 'Card sorting',
+            'description' => 'Create specific categories and list diverse
+                                elements. Organize the different elements
+                                depending on the categories available.',
+            'picture' => 'build/images/logo_workshop/card-sorting.png',
+            'type' => 'workshop',
+        ],
+        [
+            'name' => 'Hypothesis statement',
+            'description' => 'Create specific categories and list diverse
+                                elements. Organize the different elements
+                                depending on the categories available.',
+            'picture' => 'build/images/logo_workshop/card-sorting.png',
+            'type' => 'canva',
+        ],
+        [
+            'name' => 'Explorative interview',
+            'description' => 'Cover almost all user-related topics, gather
+                                informations on users’ feelings, motivations
+                                and daily routines, or how they use products.',
+            'picture' => 'build/images/logo_workshop/explorative-interview.png',
+            'type' => 'workshop',
+        ],
+        [
+            'name' => 'Problem statement',
+            'description' => 'Cover almost all user-related topics, gather
+                                informations on users’ feelings, motivations
+                                and daily routines, or how they use products.',
+            'picture' => 'build/images/logo_workshop/explorative-interview.png',
+            'type' => 'canva',
+        ],
+        [
+            'name' => 'Buy a feature',
+            'description' => 'Prioritize the features to achieve a minimum viable
+                                product. Evaluate together the different pricing of
+                                each feature.',
+            'picture' => 'build/images/logo_workshop/buy-a-feature.png',
+            'type' => 'workshop',
+        ],
+        [
+            'name' => 'Lean UX canvas',
+            'description' => 'Prioritize the features to achieve a minimum viable
+                                product. Evaluate together the different pricing of
+                                each feature.',
+            'picture' => 'build/images/logo_workshop/buy-a-feature.png',
+            'type' => 'canva',
+        ],
+        [
+            'name' => 'Storyboard',
+            'description' => 'Prioritize the features to achieve a minimum viable
+                                product. Evaluate together the different pricing of
+                                each feature.',
+            'picture' => 'build/images/logo_workshop/buy-a-feature.png',
+            'type' => 'canva',
+        ],
+        [
+            'name' => 'Heart, head, hand',
+            'description' => 'Prioritize the features to achieve a minimum viable
+                                product. Evaluate together the different pricing of
+                                each feature.',
+            'picture' => 'build/images/logo_workshop/buy-a-feature.png',
+            'type' => 'canva',
+        ],
+        [
+            'name' => 'SWOT analysis',
+            'description' => 'Prioritize the features to achieve a minimum viable
+                                product. Evaluate together the different pricing of
+                                each feature.',
+            'picture' => 'build/images/logo_workshop/buy-a-feature.png',
+            'type' => 'canva',
+        ],
+        [
+            'name' => 'Crazy eight',
+            'description' => 'Prioritize the features to achieve a minimum viable
+                                product. Evaluate together the different pricing of
+                                each feature.',
+            'picture' => 'build/images/logo_workshop/crazy-eight.png',
+            'type' => 'workshop',
+        ],
+        [
+            'name' => 'Feature cost estimation',
+            'description' => 'Prioritize the features to achieve a minimum viable
+                                product. Evaluate together the different pricing of
+                                each feature.',
+            'picture' => 'build/images/logo_workshop/buy-a-feature.png',
+            'type' => 'workshop',
+        ],
+        [
+            'name' => 'Concept testing',
+            'description' => 'Obtain rapid user feedbacks on an interface about
+                                specific usage scenarios. Use them to build concrete
+                                insights.',
+            'picture' => 'build/images/logo_workshop/concept-testing.png',
+            'type' => 'workshop',
+        ],
+        [
+            'name' => 'How might we?',
+            'description' => 'Obtain rapid user feedbacks on an interface about
+                                specific usage scenarios. Use them to build concrete
+                                insights.',
+            'picture' => 'build/images/logo_workshop/concept-testing.png',
+            'type' => 'canva',
+        ],
+        [
+            'name' => 'User testing',
+            'description' => 'Collect the opinions and feelings of users about
+                                an interface, a feature, or a service, early in
+                                the life of the project.',
+            'picture' => 'build/images/logo_workshop/user-testing.png',
+            'type' => 'workshop',
+        ],
+        [
+            'name' => 'Customer journey',
+            'description' => 'Prioritize the features to achieve a minimum viable
+                                product. Evaluate together the different pricing of
+                                each feature.',
+            'picture' => 'build/images/logo_workshop/customer-journey.png',
+            'type' => 'workshop',
+        ],
+        [
+            'name' => 'Assumtins mapping',
+            'description' => 'List assumptions by using the guide provided. Then position
+                                them to identify next your next investigation subjects.',
+            'picture' => 'build/images/logo_workshop/assumtions-mapping.png',
+            'type' => 'workshop',
+        ],
+
+    ];
+
     public function load(ObjectManager $manager): void
     {
+        $array = [];
+        foreach (self::WORKSHOPS as $workshop) {
+            $array[] = $workshop['name'];
+        }
         // $product = new Product();
         // $manager->persist($product);
 
