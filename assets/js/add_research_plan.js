7afb4879fe3ca7a97c0feb6c20526e2c23cbc48f
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-07-10 17:49:03 +0200

    Creating some usefull constants in add_research_plan.js

--- assets/js/add_research_plan.js
+++ assets/js/add_research_plan.js
@@ -3,6 +3,11 @@ if (document.querySelector('.assign-workshop')) {
     const assignWorkshopLink = document.querySelector('.assign-workshop');
     const availableWorkshopModalCloseButton = document.getElementById('research-plan-available-workshops-modal-close');
     const availableWorkshopModal = document.getElementById('research-plan-available-workshops-modal');
+    const workshopSearchbar = document.getElementById('research-plan-available-workshops-searchbar-input');
+    const availableWorkshopsCard = document.querySelectorAll('.available-workshops-card');
+    const workshopCardTitle = document.querySelectorAll('.available-workshop-name');
+    const workshopCardDescription = document.querySelectorAll('.available-workshop-description');
+    const availableWorkshopButton = document.getElementById('research-plan-available-workshops-searchbar-button');
 
     assignWorkshopLink.addEventListener('click', () => {
         availableWorkshopModal.classList.add('research-plan-available-workshops-display');
