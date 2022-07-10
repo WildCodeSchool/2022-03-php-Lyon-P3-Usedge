20101214cfa46663fd8b5de3cbb4840592ea1443
Cid-Jie <pcidjie@gmail.com>
2022-07-10 09:28:01 +0200

    USRC-38 Add research request in the route

--- src/Controller/ResearchPlanController.php
+++ src/Controller/ResearchPlanController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\ResearchRequestRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ResearchPlanController extends AbstractController
 {
-    #[Route('/research-plan', name: 'app_research_plan')]
-    public function index(): Response
+    #[Route('/research-plan/{id}', name: 'app_research_plan', methods: ['GET', 'POST'])]
+    #[Entity('researchRequest', options: ['id' => 'researchRequestId'])]
+    public function index(int $id, ResearchRequestRepository $researchRequestRepo): Response
     {
+        $request = $researchRequestRepo->findBy(['id' => $id]);
         return $this->render('research_plan/research_plan.html.twig', [
-            'controller_name' => 'ResearchPlanController',
+            'request' => $request,
         ]);
     }
 }
