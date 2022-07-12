119cdf3159df2c42a41a2d2553130d75a9e44d94
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-07-12 19:32:54 +0200

    Add some fixtures to research plan + modification of the picture of the coach in research center

---
+++ src/DataFixtures/ResearchPlanFixtures.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\ResearchPlan;
+use DateTime;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ResearchPlanFixtures extends Fixture implements DependentFixtureInterface
+{
+    private const RESEARCH_PLAN = [
+        [
+            'research_request' => 'research_request_1',
+            'coach' => 'Jason Brand',
+            'status' => 'Ongoing'
+        ],
+        [
+            'research_request' => 'research_request_6',
+            'coach' => 'Jason Brand',
+            'status' => 'Ongoing'
+        ],
+    ];
+    public function load(ObjectManager $manager): void
+    {
+        $researchPlanNumber = 1;
+        foreach (self::RESEARCH_PLAN as $researchPlanValue) {
+            $researchPlan = new ResearchPlan();
+            $date = new DateTime("now");
+            $researchPlan
+                ->setResearchRequest($this->getReference($researchPlanValue['research_request']))
+                ->setCoach($researchPlanValue['coach'])
+                ->setStatus($researchPlanValue['status'])
+                ->setCreationDate($date);
+            $this->addReference('research_plan_' . $researchPlanNumber, $researchPlan);
+            $researchPlanNumber++;
+
+            $manager->persist($researchPlan);
+        }
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+
+        return [
+
+          ResearchRequestFixtures::class,
+
+        ];
+    }
+}
